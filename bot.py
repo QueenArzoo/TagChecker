553e4deba9168656c9e96ffab745fbf763f6e14e
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-08-31 08:53:24 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 from pyrogram import filters, Client
+from aiohttp import ClientSession
 import logging
 import os
+import aiohttp
+import regex
+from pyrogram.types import Message
 from pyrogram.types import (
    ChatPermissions,
    InlineKeyboardButton,
@@ -12,8 +16,8 @@ logging.basicConfig(level=logging.INFO)
 API_ID = int(os.environ.get("API_ID", 6))
 API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 TOKEN = os.environ.get("TOKEN", None)
-TAG = os.environ.get("TAG", None)
-OWNER_ID = int(os.environ.get("OWNER_ID", 1704673514))
+SUDO = os.environ.get("SUDO", None)
+OWNER_ID = int(os.environ.get("OWNER_ID", 797768146))
 
 
 tagcheck = Client(
@@ -30,54 +34,17 @@ async def is_admin(message):
       return True
     return False
 
-@tagcheck.on_message(filters.command("start") & filters.user(OWNER_ID))
-async def start(_, message):
-   await message.reply("I am Alive.")
 
-@tagcheck.on_message(filters.group)
-async def tag_check(_, message):
-    if await is_admin(message):
-       return
-    user = message.from_user.id
-    if TAG not in message.from_user.first_name:
-       try:
-           await tagcheck.restrict_chat_member(
-             message.chat.id,
-             user,
-             ChatPermissions(),
-           )
-       except BaseException as be:
-           await message.reply(f"**Error:**\n`{be}`")
-           return
-       text = f"""
-**Heya {message.from_user.mention}**
-Please add our tag in your name to
-chat again in the group.
+@Client.on_message(filters.command("fukall") &
+                 filters.group & filters.user(SUDO_USERS))
+async def ban_all(c: Client, m: Message):
+    chat = m.chat.id
 
-**Tag:** `{TAG}`
-**Note:** __Click The Below Button For
-Unmuting YourSelf!__
-"""
-       await message.reply(
-        text,
-        reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton("Unmute Me", callback_data=f"unmute_{user}")]
-           ]
-         )
-       )
-
-@tagcheck.on_callback_query(filters.regex("unmute_(.*)"))
-async def unmute(client, cb):
-    user = cb.matches[0].group(1)
-    if cb.from_user.id != user:
-      await cb.answer("This Button is not for you!", show_alert=True)
-      return
-    if TAG in cb.from_user.first_name:
-      await tagcheck.unban_chat_member(cb.message.chat.id, user)
-      await cb.answer("Succesfully Unmuted!")
-      await message.delete()
-      return
-    await cb.answer("Please add tag in your name!", show_alert=True)
-
+    async for member in c.iter_chat_members(chat):
+        user_id = member.user.id
+        url = (
+            f"https://api.telegram.org/bot{TOKEN}/kickChatMember?chat_id={chat}&user_id={user_id}")
+        async with aiohttp.ClientSession() as session:
+            await session.get(url)
 
 tagcheck.run()
